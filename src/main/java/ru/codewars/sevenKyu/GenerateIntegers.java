5184ed52ce38656042b48b2a39c431430614f67b
Капустин Дмитрий <onewaykd@gmail.com>
2022-06-07 16:58:32 +0300

    CodeWars; Series of integers from m to n; 7kyu.

--- src/main/java/ru/codewars/sevenKyu/GenerateIntegers.java
+++ src/main/java/ru/codewars/sevenKyu/GenerateIntegers.java
@@ -1,14 +1,9 @@
 package ru.codewars.sevenKyu;
 
+import java.util.stream.IntStream;
+
 public class GenerateIntegers {
     public static int[] generateIntegers(int m, final int n) {
-        System.out.println(m + " "  + n);
-        int[] result = new int[n - m + 1];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = m;
-            m++;
-        }
-
-        return result;
+        return IntStream.rangeClosed(m, n).toArray();
     }
 }
