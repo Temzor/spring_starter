7460d0c3a80bd8d912286490a7b57f30f2ece95e
Капустин Дмитрий <onewaykd@gmail.com>
2022-06-09 14:16:31 +0300

    CodeWars; Valid Spacing; 7kyu.

--- src/main/java/ru/codewars/sevenKyu/ValidSpacing.java
+++ src/main/java/ru/codewars/sevenKyu/ValidSpacing.java
@@ -2,7 +2,6 @@ package ru.codewars.sevenKyu;
 
 public class ValidSpacing {
     public static boolean validSpacing(String s) {
-        String trim = s.trim().strip().replaceAll(" {2}", "");
-        return trim.length() >= s.length();
+        return s.trim().replace("  ", "").equals(s);
     }
 }
--- src/main/java/ru/codewars/sevenKyu/ValidSpacingTest.java
+++ src/main/java/ru/codewars/sevenKyu/ValidSpacingTest.java
@@ -1,17 +1,19 @@
 package ru.codewars.sevenKyu;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ValidSpacingTest {
     @Test
     void fixedTests() {
-        assertFalse (ValidSpacing.validSpacing("codewars "), "'codewars '");
-        assertTrue (ValidSpacing.validSpacing("Hello world"), "'Hello world'");
+        assertFalse(ValidSpacing.validSpacing("codewars "), "'codewars '");
+        assertTrue(ValidSpacing.validSpacing("Hello world"), "'Hello world'");
         assertFalse(ValidSpacing.validSpacing(" Hello world"), "' Hello world'");
         assertFalse(ValidSpacing.validSpacing("Hello  world "), "'Hello  world '");
-        assertTrue (ValidSpacing.validSpacing("Hello"), "'Hello'");
-        assertTrue (ValidSpacing.validSpacing("Helloworld"), "'Helloworld'");
+        assertTrue(ValidSpacing.validSpacing("Hello"), "'Hello'");
+        assertTrue(ValidSpacing.validSpacing("Helloworld"), "'Helloworld'");
     }
 
 }
