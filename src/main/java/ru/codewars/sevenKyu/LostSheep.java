2b1721b007d42d46850647aa525b748e271fdc86
Капустин Дмитрий <onewaykd@gmail.com>
2022-06-11 15:49:46 +0300

    CodeWars; Count all the sheep on farm in the heights of New Zealand; 7kyu.

--- src/main/java/ru/codewars/sevenKyu/LostSheep.java
+++ src/main/java/ru/codewars/sevenKyu/LostSheep.java
@@ -1,18 +1,9 @@
 package ru.codewars.sevenKyu;
 
+import static java.util.stream.IntStream.of;
+
 public class LostSheep {
     public static int lostSheep(int[] fridayNightCounting, int[] saturdayNightCounting, int sheepTotal) {
-        int sumOne = 0;
-        int sumTwo = 0;
-
-        for (int i : fridayNightCounting) {
-            sumOne += i;
-        }
-
-        for (int i : saturdayNightCounting) {
-            sumTwo += i;
-        }
-
-        return sheepTotal - sumOne - sumTwo;
+        return sheepTotal - of(fridayNightCounting).sum() - of(saturdayNightCounting).sum();
     }
 }
