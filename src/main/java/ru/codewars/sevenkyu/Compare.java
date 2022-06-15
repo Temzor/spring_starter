ce07edd05c996a550ec3de0d786e780a374d6ba0
Капустин Дмитрий <onewaykd@gmail.com>
2022-06-15 23:50:22 +0300

    CodeWars; Compare Strings by Sum of Chars; 7kyu.

--- src/main/java/ru/codewars/sevenkyu/Compare.java
+++ src/main/java/ru/codewars/sevenkyu/Compare.java
@@ -2,9 +2,6 @@ package ru.codewars.sevenkyu;
 
 public class Compare {
     public static boolean compare(String s1, String s2) {
-        int sumS1 = 0;
-        int sumS2 = 0;
-
         if (s1 == null || !s1.matches("[a-zA-Z]+")) {
             s1 = "";
         }
@@ -12,16 +9,6 @@ public class Compare {
             s2 = "";
         }
 
-        if (!s1.isEmpty()) {
-            for (char ch : s1.toUpperCase().toCharArray()) {
-                sumS1 = sumS1 + (int) ch;
-            }
-        }
-        if (!s2.isEmpty()) {
-            for (char ch : s2.toUpperCase().toCharArray()) {
-                sumS2 = sumS2 + (int) ch;
-            }
-        }
-        return sumS1 == sumS2;
+        return s1.toUpperCase().chars().sum() == s2.toUpperCase().chars().sum();
     }
 }
