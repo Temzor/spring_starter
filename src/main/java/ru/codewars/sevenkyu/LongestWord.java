736cd3ea0b3e9af531cee4195023b647b7b54198
Капустин Дмитрий <onewaykd@gmail.com>
2022-06-17 16:18:01 +0300

    CodeWars; Inspiring Strings; 7kyu.

--- src/main/java/ru/codewars/sevenkyu/LongestWord.java
+++ src/main/java/ru/codewars/sevenkyu/LongestWord.java
@@ -1,12 +1,10 @@
 package ru.codewars.sevenkyu;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class LongestWord {
     public static String longestWord(String wordString) {
-        String[] split = wordString.split(" ");
-        Arrays.sort(split, Comparator.comparingInt(String::length));
-        return split[split.length - 1];
+        return Arrays.stream(wordString.split(" "))
+                .reduce((x, y) -> x.length() <= y.length() ? y : x).get();
     }
 }
