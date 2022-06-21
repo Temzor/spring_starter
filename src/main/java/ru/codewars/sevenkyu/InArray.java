9a56ee494e23563a774660147f1a7715df1b053f
Temzor <onewaykd@gmail.com>
2022-06-21 12:34:49 +0300

    CodeWars; Which are in?; 6kyu.

--- src/main/java/ru/codewars/sevenkyu/InArray.java
+++ src/main/java/ru/codewars/sevenkyu/InArray.java
@@ -4,23 +4,11 @@ import java.util.*;
 
 public class InArray {
     public static String[] inArray(String[] array1, String[] array2) {
-        Set<String> strings = new TreeSet<>();
-
-        for (String s : array1) {
-            for (String value : array2) {
-                if (value.contains(s)) {
-                    strings.add(s);
-                }
-
-            }
-        }
-
-        for (String string : strings) {
-            System.out.println(string);
-        }
-
-
-
-        return strings.toArray(new String[0]);
+        return Arrays.stream(array1)
+                .filter(str ->
+                        Arrays.stream(array2).anyMatch(s -> s.contains(str)))
+                .distinct()
+                .sorted()
+                .toArray(String[]::new);
     }
 }
