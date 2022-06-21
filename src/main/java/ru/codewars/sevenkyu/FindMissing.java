a473d76f820716b69268153ff733ebb216607f4a
Temzor <onewaykd@gmail.com>
2022-06-21 11:39:07 +0300

    CodeWars; Find the missing term in an Arithmetic Progression; 6kyu.

--- src/main/java/ru/codewars/sevenkyu/FindMissing.java
+++ src/main/java/ru/codewars/sevenkyu/FindMissing.java
@@ -1,15 +1,9 @@
 package ru.codewars.sevenkyu;
 
+import static java.util.stream.IntStream.of;
+
 public class FindMissing {
     public static int findMissing(int[] numbers) {
-
-        int different = (numbers[numbers.length - 1] - numbers[0]) / numbers.length;
-
-        for (int i = 0; i < numbers.length - 1; i++) {
-            if (numbers[i] + different != numbers[i + 1] ) {
-                return numbers[i] + different;
-            }
-        }
-        return numbers[0];
+        return (numbers[0] + numbers[numbers.length - 1]) * (numbers.length + 1) / 2 - of(numbers).sum();
     }
 }
