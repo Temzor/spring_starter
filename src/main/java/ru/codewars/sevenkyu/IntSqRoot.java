e8b3b24709d4aa4d5451275739cb214d564206ba
Temzor <onewaykd@gmail.com>
2022-07-13 11:27:26 +0300

    CodeWars; Hero's root; 7kyu.

--- src/main/java/ru/codewars/sevenkyu/IntSqRoot.java
+++ src/main/java/ru/codewars/sevenkyu/IntSqRoot.java
@@ -2,13 +2,7 @@ package ru.codewars.sevenkyu;
 
 public class IntSqRoot {
     public static long IntRac(long n, long guess) {
-        long count = 0;
-        long digits = 0;
-        while (Math.abs(guess - digits) >= 1) {
-            digits = guess;
-            guess = (long) Math.floor((guess + n / guess) / 2);
-            count++;
-        }
-        return count;
+        long x = (guess + n / guess) / 2;
+        return Math.abs(guess - x) < 1 ? 1 : 1 + IntRac(n, x);
     }
 }
