6ca5f6b7eda025d24b0a57a1a51d061d7cce0b88
Temzor <onewaykd@gmail.com>
2022-07-25 18:50:56 +0300

    Frame for "4. Аттестация [#504878]".

---
+++ src/main/java/com/attestaion/Pupil.java
@@ -0,0 +1,6 @@
+package com.attestaion;
+
+import java.util.List;
+
+public record Pupil(String name, List<Subject> subjects) {
+}
---
+++ src/main/java/com/attestaion/Subject.java
@@ -0,0 +1,5 @@
+package com.attestaion;
+
+public record Subject(String name, int score) {
+}
+
