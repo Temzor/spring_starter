7ee9e831ff61e211cac1b57574ed034664d98388
Капустин Дмитрий <onewaykd@gmail.com>
2022-07-28 21:42:22 +0300

    УРОК 45. Удаление Java объектов из БД.

---
+++ src/main/java/com/hibernate_test/TestDeletedHQL.java
@@ -0,0 +1,28 @@
+package com.hibernate_test;
+
+import com.hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class TestDeletedHQL {
+    public static void main(String[] args) {
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory()) {
+
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            Employee employee = session.get(Employee.class, 1);
+            session.delete(employee);
+
+            session.createQuery("delete Employee WHERE name = 'Alexandr'").executeUpdate();
+
+            session.getTransaction().commit();
+            System.out.println("Done.");
+
+        }
+    }
+}
