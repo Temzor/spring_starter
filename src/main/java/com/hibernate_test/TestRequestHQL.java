b8acbd7cc3b4523199321fa39a09510b9fc1c4f0
Капустин Дмитрий <onewaykd@gmail.com>
2022-07-28 21:16:13 +0300

    УРОК 43. Получение Java объектов из БД.

---
+++ src/main/java/com/hibernate_test/TestRequestHQL.java
@@ -0,0 +1,48 @@
+package com.hibernate_test;
+
+import com.hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class TestRequestHQL {
+    public static void main(String[] args) {
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory()) {
+
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            var employees = session.createQuery("from Employee")
+                            .getResultList();
+
+            for (Object employee : employees) {
+                System.out.println(employee);
+            }
+
+
+            var employees1 = session.createQuery("from Employee WHERE name = 'Alexandr'")
+                    .getResultList();
+
+            for (Object employee : employees1) {
+                System.out.println(employee);
+            }
+
+
+            var employees2 = session.createQuery("from Employee WHERE salary > 650000")
+                    .getResultList();
+
+            for (Object employee : employees2) {
+                System.out.println(employee);
+            }
+
+
+            session.getTransaction().commit();
+            System.out.println("Done.");
+        }
+    }
+}
--- src/main/java/com/hibernate_test/TestRequestSQL.java
+++ src/main/java/com/hibernate_test/TestRequestSQL.java
@@ -1,12 +1,11 @@
 package com.hibernate_test;
 
-
 import com.hibernate_test.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class TestHibernate {
+public class TestRequestSQL {
     public static void main(String[] args) {
         try (SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -14,11 +13,16 @@ public class TestHibernate {
                 .buildSessionFactory()) {
 
             Session session = factory.getCurrentSession();
-            Employee employee = new Employee("Alexandr", "Ivanov", "IT", 600_000);
+            Employee employee = new Employee("Oleg", "Sidorov", "HR", 700_000);
 
             session.beginTransaction();
             session.save(employee);
+
+            int myId = employee.getId();
+            Employee employee1 = session.get(Employee.class, myId);
             session.getTransaction().commit();
+
+            System.out.println(employee1);
 
             System.out.println("Done.");
         }
