0e7cf7d786219aa3760dca80e906295969067571
Dmitrii <46348969+Temzor@users.noreply.github.com>
2022-08-02 10:58:29 +0300

    Delete src/main/java/com/test directory

--- src/main/java/com/test/skillUp/ThreadTestRunnable2.java
+++
@@ -1,9 +0,0 @@
-package com.test.skillUp;
-
-public class ThreadTestRunnable2 {
-    public static void main(String[] args) {
-        new Thread(() -> System.out.println("Hi!")).start();
-
-        new Thread(() -> System.out.println("Hi? you!")).start();
-    }
-}
