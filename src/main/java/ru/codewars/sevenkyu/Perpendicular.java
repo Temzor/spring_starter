680f03c96c44efe64623c8f7ee00f70357217279
d.kapustin <onewaykd@gmail.com>
2022-12-25 23:51:43 +0300

    CodeWars; Perpendicular lines; Community variant; 7kyu.

---
+++ src/main/java/ru/codewars/sevenkyu/Perpendicular.java
@@ -0,0 +1,7 @@
+package ru.codewars.sevenkyu;
+
+public class Perpendicular {
+    public static int maxBisectors(int n) {
+        return (int) Math.floor(n * n / 4);
+    }
+}
---
+++ src/test/java/ru/codewars/sevenkyu/PerpendicularTest.java
@@ -0,0 +1,19 @@
+package ru.codewars.sevenkyu;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PerpendicularTest {
+    @Test
+    public void basicTests() {
+        assertThat(0).isEqualTo(Perpendicular.maxBisectors(0));
+        assertThat(0).isEqualTo(Perpendicular.maxBisectors(1));
+        assertThat(1).isEqualTo(Perpendicular.maxBisectors(2));
+        assertThat(2).isEqualTo(Perpendicular.maxBisectors(3));
+        assertThat(4).isEqualTo(Perpendicular.maxBisectors(4));
+        assertThat(6).isEqualTo(Perpendicular.maxBisectors(5));
+        assertThat(9).isEqualTo(Perpendicular.maxBisectors(6));
+        assertThat(650).isEqualTo(Perpendicular.maxBisectors(51));
+    }
+}
