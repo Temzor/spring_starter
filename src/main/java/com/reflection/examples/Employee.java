ecdd9437222e3396991c17d811b4bd9e96d14f77
d.kapustin <onewaykd@gmail.com>
2023-01-09 12:30:29 +0300

    refactor spring_starter/src/main/java/com/reflection/examples/Employee.java:27:0

--- src/main/java/com/reflection/examples/Employee.java
+++ src/main/java/com/reflection/examples/Employee.java
@@ -4,7 +4,7 @@ public class Employee {
     public int id;
     public String name;
     public String department;
-    private double salary = 1000;
+    private final double salary = 1000;
 
     public Employee() {
 
@@ -16,22 +16,8 @@ public class Employee {
         this.department = department;
     }
 
-//    public Employee(int id, String name, String department, double salary) {
-//        this.id = id;
-//        this.name = name;
-//        this.department = department;
-//        this.salary = salary;
-//    }
-
-//    public double getSalary() {
-//        return salary;
-//    }
-//
-//    public void setSalary(double salary) {
-//        this.salary = salary;
-//    }
-
-    private void changeDepartment(String newDepartment) {
+    /*
+private void changeDepartment(String newDepartment) {
         department = newDepartment;
         System.out.println("New department is: " + department);
     }
@@ -49,4 +35,5 @@ public class Employee {
                 + ", salary=" + salary
                 + '}';
     }
+     */
 }
